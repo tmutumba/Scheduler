7dd156cec9625663f6400ffd825d9e6421efc080
tmutumba <mutumbatonny@gmail.com>
2019-07-11 15:18:28 -0600

    Assignment 4

--- CPU.c
+++ CPU.c
@@ -11,60 +11,6 @@
 #define NUM_SECONDS 20
 #define ever ;;
 
-/*
-This program does the following.
-1) Creates handlers for two signals.
-2) Create an idle process which will be executed when there is nothing
-   else to do.
-3) Create a send_signals process that sends a SIGALRM every so often.
-
-The output looks approximately like:
-
-[...]
-Sending signal:  14 to process: 35050
-Stopping: 35052
----- entering scheduler
-Continuing idle: 35052
----- leaving scheduler
-Sending signal:  14 to process: 35050
-at the end of send_signals
-Stopping: 35052
----- entering scheduler
-Continuing idle: 35052
----- leaving scheduler
----- entering process_done
-Timer died, cleaning up and killing everything
-Terminated: 15
-
----------------------------------------------------------------------------
-Add the following functionality.
-1) Change the NUM_SECONDS to 20.
-
-2) Take any number of arguments for executables and place each on the
-   processes list with a state of NEW. The executables will not require
-   arguments themselves.
-
-3) When a SIGALRM arrives, scheduler() will be called; it currently simply
-   restarts the idle process. Instead, do the following.
-   a) Update the PCB for the process that was interrupted including the
-      number of context switches and interrupts it had and changing its
-      state from RUNNING to READY.
-   b) If there are any NEW processes on processes list, change its state to
-      RUNNING, and fork() and execl() it.
-   c) If there are no NEW processes, round robin the processes in the
-      processes queue that are READY. If no process is READY in the
-      list, execute the idle process.
-
-4) When a SIGCHLD arrives notifying that a child has exited, process_done() is
-   called.
-   a) Add the printing of the information in the PCB including the number
-      of times it was interrupted, the number of times it was context
-      switched (this may be fewer than the interrupts if a process
-      becomes the only non-idle process in the ready queue), and the total
-      system time the process took.
-   b) Change the state to TERMINATED.
-   c) Restart the idle process to use the rest of the time slice.
-*/
 
 enum STATE { NEW, RUNNING, WAITING, READY, TERMINATED, EMPTY };
 
